a6757706644db4a07152182186fba7c7df9e8d0d
suke <2568597007@qq.com>
2020-12-17 15:47:57 +0800

    config package no error result

--- config/config.go
+++ config/config.go
@@ -8,15 +8,14 @@ import (
 )
 
 // ReadConfig ReadConfig
-func ReadConfig(data interface{}, path string) (err error) {
+func ReadConfig(data interface{}, path string) {
 	f, err := os.Open(path)
 	defer f.Close()
 	if err != nil {
 		log.Fatalln("打开配置文件错误，请创建!")
-		return io.EOF
+		panic(io.EOF)
 	}
 	if err = json.NewDecoder(f).Decode(data); err != nil {
-		return err
+		panic(err)
 	}
-	return nil
 }
