b32385db8982843dc6e0b060a219290ff7e77984
suke <2568597007@qq.com>
2021-04-14 16:31:16 +0800

    分离app基本配置

---
+++ app.go
@@ -0,0 +1,48 @@
+package gowebtemplate
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lazyfury/go-web-template/response"
+	"github.com/lazyfury/go-web-template/tools"
+)
+
+// New 初始化
+func New() *gin.Engine {
+	g := gin.New()
+
+	// 跨域配置
+	g.Use(tools.DefaultCors)
+
+	// 自定义recover
+	g.Use(response.Recover)
+
+	// HandleMethodNotAllowed
+	g.HandleMethodNotAllowed = true
+	g.NoMethod(func(c *gin.Context) {
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		response.Error(response.NoMethod)
+	})
+
+	// handle 路由不存在
+	g.NoRoute(func(c *gin.Context) {
+		if c.Request.URL.Path != "/favicon.ico" {
+			response.Error(response.NoRoute)
+		}
+	})
+
+	// 移除多余斜杠 /api//v1/doSomething/ => /api/v1/doSomething
+	g.RemoveExtraSlash = true
+	// 重定向请求移除斜杠请求
+	g.RedirectTrailingSlash = true
+
+	// 错误码配置
+	response.RecoverErrHtml = true
+	response.RecoverErrTemplateName = "err/error.html"
+
+	return g
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/aliyun/aliyun-oss-go-sdk v2.1.5+incompatible
+	github.com/gin-contrib/static v0.0.1 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/google/uuid v1.1.4
 	github.com/satori/go.uuid v1.2.0 // indirect
